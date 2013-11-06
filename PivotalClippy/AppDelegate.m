5131a9b8e95b3f12bbda130ab86d646afc1a50c8
Andrew Bruce <andrew@camelpunch.com>
2013-11-06 00:06:50 -0800

    Initial Commit

---
+++ PivotalClippy/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  PivotalClippy
+//
+//  Created by Andrew Bruce on 11/6/13.
+//  Copyright (c) 2013 Andrew Bruce. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ PivotalClippyTests/PivotalClippyTests.m
@@ -0,0 +1,34 @@
+//
+//  PivotalClippyTests.m
+//  PivotalClippyTests
+//
+//  Created by Andrew Bruce on 11/6/13.
+//  Copyright (c) 2013 Andrew Bruce. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PivotalClippyTests : XCTestCase
+
+@end
+
+@implementation PivotalClippyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
