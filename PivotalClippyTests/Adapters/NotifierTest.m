32f17547357dc37a1e68efdb3b596ce9343f3733
Andrew Bruce <andrew@camelpunch.com>
2013-11-09 17:21:53 -0800

    Back-fill Notifier test

---
+++ PivotalClippyTests/Adapters/NotifierTest.m
@@ -0,0 +1,25 @@
+#import <XCTest/XCTest.h>
+#import "Notifier.h"
+
+@interface NotifierTest : XCTestCase
+@end
+
+@implementation NotifierTest {
+    NSUserNotificationCenter *center;
+    Notifier *notifier;
+}
+
+- (void)testDeliversUserNotification
+{
+    center = [NSUserNotificationCenter defaultUserNotificationCenter];
+    [center removeAllDeliveredNotifications];
+    notifier = [[Notifier alloc] initWithNotificationCenter:center];
+    [notifier repository:nil didPutItem:@"some string"];
+
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:
+                              @"subtitle == 'some string copied to clipboard'"];
+    NSArray *matchingNotifications = [center.deliveredNotifications filteredArrayUsingPredicate:predicate];
+    XCTAssertEqualObjects(@1, @(matchingNotifications.count));
+}
+
+@end
